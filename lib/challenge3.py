804ee5ff0fce24e33281271d90884b3582baac43
Nurdin Ismail <nurdin.ismail@student.moringaschool.com>
2023-08-21 14:05:06 +0300

    almost done

--- lib/challenge3.py
+++ lib/challenge3.py
@@ -19,3 +19,92 @@
 # The consonant substrings are: "str" and "ngth" with values 
 # "str" = 19 + 20 + 18 = 57 and "ngth" = 14 + 7 + 20 + 8 = 49. 
 # The highest is 57.
+
+
+def consonant_value(str1):
+    alphabetic_number_position = {
+    "a": 1,
+    "b": 2,
+    "c": 3,
+    "d": 4,
+    "e": 5,
+    "f": 6,
+    "g": 7,
+    "h": 8,
+    "i": 9,
+    "j": 10,
+    "k": 11,
+    "l": 12,
+    "m": 13,
+    "n": 14,
+    "o": 15,
+    "p": 16,
+    "q": 17,
+    "r": 18,
+    "s": 19,
+    "t": 20,
+    "u": 21,
+    "v": 22,
+    "w": 23,
+    "x": 24,
+    "y": 25,
+    "z": 26,
+}
+
+    vowels = "aeiou"
+
+    #split string by character.
+
+    print('a' in vowels)
+    print('u' in vowels)
+    print('b' in vowels)
+    print('a' in vowels)
+
+    #replace vowels with empty space
+    
+    
+    test_str = str1
+    res2 = []
+    for j in test_str:
+        print(j)
+        if j in vowels:
+          test_str = test_str.replace(j,"*")
+          res=test_str.split('*')
+         
+          
+         
+          print("The splitted string : " + str(res))
+        
+          result = ' '.join(res)
+          res2 = result.split(' ')
+          res2.pop()
+          print(result)
+          print(res2)
+        else:
+            continue
+           
+        
+    
+            
+    print(res2)   
+
+    for j in res2:
+        # if j == alphabetic_number_position[j]:
+        #     print(alphabetic_number_position[j])
+        print(j)
+        # print(alphabetic_number_position[j])
+
+        if j in alphabetic_number_position:
+            print(True)
+
+ 
+        
+ 
+        
+
+
+
+
+    
+
+consonant_value('nightingale')
